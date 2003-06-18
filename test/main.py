c1ed0949b71f01fdbf0d75158512abb8ef97eb9e
wrobell <wrobell@pld-linux.org>
2003-06-18 21:30:57 +0000

    - some docs

--- test/main.py
+++ test/main.py
@@ -1,12 +1,17 @@
-# $Id: main.py,v 1.1 2003/06/18 21:27:03 wrobell Exp $
+# $Id: main.py,v 1.2 2003/06/18 21:30:57 wrobell Exp $
 
 import unittest
 import logging
 import logging.config
 
+"""
+<s>Run all bazaar test modules.</s>
+"""
+
 logging.config.fileConfig('log.ini')
 log = logging.getLogger('bazaar.test')
 
 if __name__ == '__main__':
+    # import all test modules
     import conf
     unittest.main(conf)
