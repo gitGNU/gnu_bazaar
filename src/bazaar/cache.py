47d174245d44fe0ed9cb78698f080fac62b623ec
wrobell <wrobell@pld-linux.org>
2003-07-10 23:14:15 +0000

    - added append and remove methods - getObjects = values method alias created

--- src/bazaar/cache.py
+++ src/bazaar/cache.py
@@ -1,4 +1,4 @@
-# $Id: cache.py,v 1.1 2003/06/27 22:25:21 wrobell Exp $
+# $Id: cache.py,v 1.2 2003/07/10 23:14:15 wrobell Exp $
 
 import logging
 
@@ -10,9 +10,27 @@ log = logging.getLogger('bazaar.cache')
 </p>
 """
 
-class Cache:
+class Cache(dict):
     """
-    <s></s>
+    <s>Abstract, basic class for different object caches.</s>
     """
     def __init__(self):
-        pass
+        """
+        <s>Create object cache.</s>
+        """
+        dict.__init__(self)
+        self.getObjects = self.values
+
+
+    def append(self, obj):
+        """
+        <s>Append object to cache.</s>
+        """
+        self[obj.key] = obj
+
+
+    def remove(self, obj):
+        """
+        <s>Remove object from cache.</s>
+        """
+        del self[obj.key]
