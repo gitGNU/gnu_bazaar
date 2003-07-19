e2741f2d386658c9ef0b55e8bc9b412b298a589a
wrobell <wrobell@pld-linux.org>
2003-07-19 10:01:59 +0000

    - set article and order item classes key - missing columns added

--- test/app.py
+++ test/app.py
@@ -1,7 +1,10 @@
-# $Id: app.py,v 1.2 2003/07/17 23:29:46 wrobell Exp $
+# $Id: app.py,v 1.3 2003/07/19 10:01:59 wrobell Exp $
 
 """
 <s>Sample test application for Bazaar layer testing purposes.</s>
+<p>
+    File sql/init.sql contains relations definitions.
+</p>
 """
 
 import bazaar.conf
@@ -27,8 +30,11 @@ Employee.setKey(('name', 'surname'))
 Article = bazaar.conf.Persitence('Article', relation = 'article')
 Article.addColumn('name')
 Article.addColumn('price')
+Article.setKey(('name',))
 
 OrderItem = bazaar.conf.Persitence('OrderItem', relation = 'order_item')
+OrderItem.addColumn('order')
 OrderItem.addColumn('pos')
 OrderItem.addColumn('quantity')
 #OrderItem.addColumn('article')
+OrderItem.setKey(('order', 'pos'))
