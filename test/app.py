6ebea1b3f3829e46995b6c1a866aa76e7b7af865
wrobell <wrobell@pld-linux.org>
2003-08-07 17:49:49 +0000

    - uncommented article column of order item class

--- test/app.py
+++ test/app.py
@@ -1,4 +1,4 @@
-# $Id: app.py,v 1.3 2003/07/19 10:01:59 wrobell Exp $
+# $Id: app.py,v 1.4 2003/08/07 17:49:49 wrobell Exp $
 
 """
 <s>Sample test application for Bazaar layer testing purposes.</s>
@@ -36,5 +36,5 @@ OrderItem = bazaar.conf.Persitence('OrderItem', relation = 'order_item')
 OrderItem.addColumn('order')
 OrderItem.addColumn('pos')
 OrderItem.addColumn('quantity')
-#OrderItem.addColumn('article')
+OrderItem.addColumn('article')
 OrderItem.setKey(('order', 'pos'))
