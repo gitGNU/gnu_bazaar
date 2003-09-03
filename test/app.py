6e86e0dd67a2203940f5452bd1fc1cacc47581f3
wrobell <wrobell@pld-linux.org>
2003-09-03 22:30:38 +0000

    - define OrderItem <-> Article one-to-one association - typos

--- test/app.py
+++ test/app.py
@@ -1,4 +1,4 @@
-# $Id: app.py,v 1.5 2003/08/27 15:02:47 wrobell Exp $
+# $Id: app.py,v 1.6 2003/09/03 22:30:38 wrobell Exp $
 
 """
 Sample test application for Bazaar layer testing purposes.
@@ -12,28 +12,28 @@ db_module = None
 dsn = ''
 
 # define test application classes
-Order = bazaar.conf.Persitence('Order', relation = 'order')
+Order = bazaar.conf.Persistence('Order', relation = 'order')
 Order.addColumn('no')
 Order.addColumn('finished')
 #Order.addColumn('items')
 #Order.addColumn('employees')
 Order.setKey(('no', ))
 
-Employee = bazaar.conf.Persitence('Employee', relation = 'employee')
+Employee = bazaar.conf.Persistence('Employee', relation = 'employee')
 Employee.addColumn('name')
 Employee.addColumn('surname')
 Employee.addColumn('phone')
 #Employee.addColumn('orders')
 Employee.setKey(('name', 'surname'))
 
-Article = bazaar.conf.Persitence('Article', relation = 'article')
+Article = bazaar.conf.Persistence('Article', relation = 'article')
 Article.addColumn('name')
 Article.addColumn('price')
 Article.setKey(('name',))
 
-OrderItem = bazaar.conf.Persitence('OrderItem', relation = 'order_item')
+OrderItem = bazaar.conf.Persistence('OrderItem', relation = 'order_item')
 OrderItem.addColumn('order')
 OrderItem.addColumn('pos')
 OrderItem.addColumn('quantity')
-OrderItem.addColumn('article')
+OrderItem.addColumn('article_fkey', 'article', Article, ('article_fkey',))
 OrderItem.setKey(('order', 'pos'))
