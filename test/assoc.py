7883e568ee9e8b3113776f815dad28678a30105a
wrobell <wrobell@pld-linux.org>
2003-09-07 11:49:30 +0000

    - support for current one-to-one association code

--- test/assoc.py
+++ test/assoc.py
@@ -1,4 +1,4 @@
-# $Id: assoc.py,v 1.1 2003/09/03 22:48:40 wrobell Exp $
+# $Id: assoc.py,v 1.2 2003/09/07 11:49:30 wrobell Exp $
 
 import app
 import btest
@@ -12,16 +12,20 @@ class OneToOneAssociationTestCase(btest.DBBazaarTestCase):
         """Test one-to-one association loading"""
 
         self.bazaar.getObjects(app.OrderItem)
-        self.bazaar.getObjects(app.Article) # fixme: to be removed
         dbc = self.bazaar.motor.dbc
         dbc.execute('select "order", pos, article_fkey from order_item')
         row = dbc.fetchone()
         while row:
             order_item = self.bazaar.brokers[app.OrderItem].cache[(row[0], row[1])]
+
+            # check the value of foreign key
             self.assertEqual(order_item.article_fkey, row[2], \
                 'article foreign key mismatch "%s" != "%s"' % (order_item.article_fkey, row[2]))
+
+            # check the value of associated object's primary key
             self.assertEqual(order_item.article.key, row[2], \
                 'article primary key mismatch "%s" != "%s"' % (order_item.article.key, row[2]))
+
             row = dbc.fetchone()
 
 
@@ -44,4 +48,17 @@ class OneToOneAssociationTestCase(btest.DBBazaarTestCase):
         self.bazaar.update(order_item)
 
         order_item.article = None
-        self.bazaar.update(order_item)
+        self.assertRaises(app.db_module.ProgrammingError, self.bazaar.update, order_item)
+        self.bazaar.rollback() # exception line above, so rollback
+
+        art = app.Article()
+        art.name = 'xxx3'
+        art.price = 2.3
+
+        oi = app.OrderItem()
+        oi.order = 0
+        oi.pos = 1000
+        oi.article = art
+        oi.quantity = 5
+        self.bazaar.add(art)
+        self.bazaar.add(oi)
