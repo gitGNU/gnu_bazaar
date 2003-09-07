a5bcdc5dfe9b4ac2dfce03db486dbcfc5daa995c
wrobell <wrobell@pld-linux.org>
2003-09-07 11:46:54 +0000

    - abstract association reference proxy class to support one-to-one,   one-to-many and many-to-many associations

--- src/bazaar/assoc.py
+++ src/bazaar/assoc.py
@@ -1,4 +1,4 @@
-# $Id: assoc.py,v 1.1 2003/09/03 22:48:40 wrobell Exp $
+# $Id: assoc.py,v 1.2 2003/09/07 11:46:54 wrobell Exp $
 """
 Association classes.
 """
@@ -7,74 +7,195 @@ import logging
 
 log = logging.getLogger('bazaar.assoc')
 
-#class Association(object):
-#    """
-#    Basic association descriptor.
-#
-#    @ivar cls: Associated application object class.
-#    """
-#    def __init__(self, cls):
-#        """
-#        Initialize association descriptor.
-#
-#        @param cls: Associated application object class.
-#        """
-#        self.cls = cls
-
-
-#class OneToOneAssociation(Association):
-class OneToOneAssociation(object):
+class AssociationReferenceProxy(dict):
     """
-    One to one association descriptor.
+    Association reference proxy abstract class for application objects. 
 
-    @ivar column: Application class column which defines this association.
-    @ivar broker:
+    Reference proxy allows to get (upon foreign key value of associating
+    object's column) and set (upon primary key value of associated object),
+    reference of associated object.
 
-    @see: L{bazaar.assoc.Association}
+    There should be one reference proxy object per association between
+    application classes.
+    
+    It is allowed to set reference of:
+        - associated object with primary key
+        - associated object without primary key (object is not completed nor
+          stored in database)
+        - None (NULL) value
+
+    When associated object has no primary key, then reference proxy buffers
+    the object as value. Objects are buffered with buffer key, which is
+    defined by descendants of this class (see L{getForeignKey} and
+    L{setForeignKey} methods). The buffer key value should be unique in
+    space of all objects of associating class, i.e. for one-to-one
+    association buffer key can be associating object or for one-to-many
+    buffer key can be tuple of associating object and object's position in
+    the list of associated objects.
+
+    Application class column defines the name of foreign key column and
+    associated application class.
+
+    @ivar column: Associating application class column.
+    @ivar broker: Broker of associated application class.
+
+    @see: L{bazaar.conf.Persistence} L{bazaar.conf.Column}
     """
     def __init__(self, column):
         """
-        Create one to one association descriptor.
+        Initialize association reference proxy.
+
+        Application broker is not initialized with the constructor, but is
+        set when Bazaar layer is started up.
+
+        @param column: Associating application class column.
+
+        @see: L{bazaar.core.Bazaar.__init__} L{bazaar.conf.Persistence}
+            L{bazaar.conf.Column}
+        """
+        super(AssociationReferenceProxy, self).__init__()
+        self.column = column
+
+
+    def __getitem__(self, buffer_key):
+        """
+        Return reference of associated object.
+
+        Foreign key value of associating object's column is extracted with 
+        L{bazaar.assoc.AssociationReferenceProxy.getForeignKey} method.
+
+        @param buffer_key: Buffer key value.
+
+        @see: L{bazaar.assoc.AssociationReferenceProxy.getForeignKey}
+        """
+        assert self.broker is not None
+
+        # if the associated object have had no primary key, then return
+        # object from reference buffer, otherwise get associated object
+        # with broker
+        if dict.has_key(self, buffer_key):
+            return dict.__getitem__(self, buffer_key)
+        else:
+            value_key = self.getForeignKey(buffer_key)
+            if value_key is None:
+                return None
+            else:
+                return self.broker.get(value_key)
+
+
+    def __setitem__(self, buffer_key, value):
+        """
+        Set reference of associated object.
+
+        Foreign key value of associating object's column is set with 
+        L{bazaar.assoc.AssociationReferenceProxy.setForeignKey} method.
+
+        @param buffer_key: Buffer key value.
+        @param value: Associated object.
+
+        @see: L{bazaar.assoc.AssociationReferenceProxy.setForeignKey}
+        """
+        # if value is None then just set associating object foreign key
+        # column value to None
+        if value is None:
+            self.setForeignKey(buffer_key, None)
+            if dict.has_key(self, buffer_key):
+                del self[buffer_key]
+        else:
+            if value.key is None:
+                # associated object has no primary key, so store object in reference buffer
+                dict.__setitem__(self, buffer_key, value)
+            else:
+                # associated object has primary key
+                # if the object exists in reference buffer, then remove it
+                if dict.has_key(self, buffer_key):
+                    del self[buffer_key]
+
+            # finally, assign associated object primary key to associating
+            # object column foreign key value
+            self.setForeignKey(buffer_key, value.key)
+
+
+    def getForeignKey(self, buffer_key):
+        """
+        Abstract method to get foreign key value of associating
+        object's column.
+
+        @param buffer_key: Buffer key value.
+        """
+        raise NotImplementedError
+
+
+    def setForeignKey(self, buffer_key, value_key):
+        """
+        Abstract method to set foreign key value of associating
+        object's column with associated object's primary key.
+
+        @param buffer_key: Buffer key value.
+        @param value_key: Associated object's primary key value.
+        """
+        raise NotImplementedError
+
+
+
+class OneToOneAssociation(AssociationReferenceProxy):
+    """
+    One to one association descriptor and reference proxy.
+
+    @see: L{bazaar.assoc.AssociationReferenceProxy}
+    """
+    def __init__(self, column):
+        """
+        Create one to one association descriptor and reference proxy.
 
         @param column: Application class column.
+
+        @see: L{bazaar.assoc.AssociationReferenceProxy.__init__}
         """
-        self.column = column
-        self.broker = None
-        self.buffor = {}
+        super(OneToOneAssociation, self).__init__(column)
 
 
     def __get__(self, obj, cls):
         """
-        """
-        if not obj: return self
-        assert self.broker is not None
+        Descriptor method to get reference of associated object.
 
-        # if the associated object have had no key, then return it from
-        # reference buffor; otherwise get it from object cache
-        if obj in self.buffor:
-            return buffor[obj]
+        @param obj: Associating object.
+        @param cls: Associating class.
+
+        @return: Returns associated object when C{obj} is not null,
+            else descriptor object is returned.
+        """
+        if obj:
+            return self[obj]
         else:
-            return self.broker.cache[getattr(obj, self.column.name)]
+            return self
 
 
     def __set__(self, obj, value):
         """
+        Descriptor method to set associating object's column value.
+
+        @param obj: Associating object.
+        @param value: Associated object.
         """
-        # if value is None then just set the value
-        if value is None:
-            setattr(obj, self.column.name, None)
-            return
+        self[obj] = value
 
-        assert value is not None
 
-        # check associated object key
-        if value.key is None:
-            # if key is None then store object in reference buffor
-            self.buffor[obj] = value
-        else:
-            # if key is not None then check if the object exists in reference
-            # buffor and when it is true remove it from the buffor
-            if obj in self.buffor:
-                del self.buffor[obj]
-            # finally, assign associated object key to associating object column
-            setattr(obj, self.column.name, value.key)
+    def getForeignKey(self, buffer_key):
+        """
+        Return foreign key value of associating object's column.
+
+        @param buffer_key: Buffer key value, which is associating object.
+        """
+        return getattr(buffer_key, self.column.name)
+
+
+    def setForeignKey(self, buffer_key, value_key):
+        """
+        Set foreign key value of associating object's column to primary key
+        of associated object.
+
+        @param buffer_key: Buffer key value, which is associating object.
+        @param value_key: Associated object's primary key value.
+        """
+        setattr(buffer_key, self.column.name, value_key)
