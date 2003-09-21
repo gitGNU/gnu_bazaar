5e1188963d731823f8dbd14a38af6a169a66ab51
wrobell <wrobell@pld-linux.org>
2003-09-21 00:56:30 +0000

    - support for new object key idea

--- src/bazaar/cache.py
+++ src/bazaar/cache.py
@@ -1,4 +1,4 @@
-# $Id: cache.py,v 1.3 2003/08/25 19:01:06 wrobell Exp $
+# $Id: cache.py,v 1.4 2003/09/21 00:56:30 wrobell Exp $
 """
 Cache classes for application objects.
 """
@@ -23,11 +23,11 @@ class Cache(dict):
         """
         Append object to cache.
         """
-        self[obj.key] = obj
+        self[obj.__key__] = obj
 
 
     def remove(self, obj):
         """
         Remove object from cache.
         """
-        del self[obj.key]
+        del self[obj.__key__]
