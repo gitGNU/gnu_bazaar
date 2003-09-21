f5e7bb675fc430aeff9312141c6dc87f597f1a9f
wrobell <wrobell@pld-linux.org>
2003-09-21 00:41:48 +0000

    - support for one-to-many and one-to-one associations

--- src/bazaar/assoc.py
+++ src/bazaar/assoc.py
@@ -1,4 +1,4 @@
-# $Id: assoc.py,v 1.6 2003/09/19 14:59:48 wrobell Exp $
+# $Id: assoc.py,v 1.7 2003/09/21 00:41:48 wrobell Exp $
 """
 Association classes.
 """
@@ -11,68 +11,78 @@ class AssociationReferenceProxy(dict):
     """
     Association reference proxy abstract class for application objects. 
 
-    Reference proxy allows to get (upon foreign key value of associating
-    object's column) and set (upon primary key value of associated object),
-    reference of associated object.
+    Reference proxy allows to get (upon foreign key value of object's column)
+    and set (upon primary key value of referenced object) reference to
+    other object.
 
     There should be one reference proxy object per association between
     application classes.
     
-    It is allowed to set reference of:
-        - associated object with primary key
-        - associated object without primary key (object is not completed nor
+    It is allowed to set reference to:
+        - application object with primary key
+        - application object without primary key (object is not completed nor
           stored in database)
         - None (NULL) value
 
-    When associated object has no primary key, then reference proxy buffers
+    When referenced object has no primary key, then reference proxy buffers
     the object as value. Objects are buffered with buffer key, which is
     defined by descendants of this class (see L{getForeignKey} and
     L{setForeignKey} methods). The buffer key value should be unique in
-    space of all objects of associating class, i.e. for one-to-one
-    association buffer key can be associating object or for one-to-many
-    buffer key can be tuple of associating object and object's position in
-    the list of associated objects.
+    space of all referenced objects of given class, i.e. for one-to-one
+    association buffer key can be referenced object itself and for one-to-many
+    buffer key can be tuple of object and referenced object's position in
+    the list of objects.
 
-    Application class column defines the name of foreign key column and
-    associated application class.
+    The class derives from C{dict} type, so setting and getting referenced object is
+    performed with::
 
-    @ivar column: Associating application class column.
-    @ivar broker: Broker of associated application class.
+        ref_buffer[obj] = value
+        value = ref_buffer[obj]
+
+    Application class attribute C{col} defines parameters of association.
+
+    @ivar col: Application object's class attribute.
+    @ivar broker: Broker of application class.
+    @ivar vbroker: Broker of referenced application objects' class.
+    @ivar association: Referenced object's association object of
+        bi-directional association.
 
     @see: L{bazaar.conf.Persistence} L{bazaar.conf.Column}
     """
-    def __init__(self, column):
+    def __init__(self, col):
         """
-        Initialize association reference proxy.
+        Create association reference proxy.
 
-        Application broker is not initialized with the constructor, but is
+        Brokers are not initialized with the constructor. Instead, they are
         set when Bazaar layer is started up.
 
-        @param column: Associating application class column.
+        @param col: Application object's class attribute.
 
         @see: L{bazaar.core.Bazaar.__init__} L{bazaar.conf.Persistence}
             L{bazaar.conf.Column}
         """
         super(AssociationReferenceProxy, self).__init__()
-        self.column = column
+        self.col = col
         self.broker = None
+        self.vbroker = None
+        self.association = None
 
 
     def __getitem__(self, buffer_key):
         """
-        Return reference of associated object.
+        Return referenced object.
 
-        Foreign key value of associating object's column is extracted with 
+        Foreign key value of referenced object's column is extracted with 
         L{getForeignKey} method.
 
         @param buffer_key: Buffer key value.
 
         @see: L{getForeignKey}
         """
-        assert self.broker is not None
+        assert self.vbroker is not None
 
-        # if the associated object have had no primary key, then return
-        # object from reference buffer, otherwise get associated object
+        # if the referenced object have had no primary key, then return
+        # object from reference buffer, otherwise get referenced object
         # with broker
         if dict.has_key(self, buffer_key):
             return dict.__getitem__(self, buffer_key)
@@ -81,18 +91,18 @@ class AssociationReferenceProxy(dict):
             if value_key is None:
                 return None
             else:
-                return self.broker.get(value_key)
+                return self.vbroker.get(value_key)
 
 
     def __setitem__(self, buffer_key, value):
         """
-        Set reference of associated object.
+        Assign referenced object.
 
-        Foreign key value of associating object's column is set with 
+        Foreign key value of referenced object's column is set with 
         L{setForeignKey} method.
 
         @param buffer_key: Buffer key value.
-        @param value: Associated object.
+        @param value: Referenced object.
 
         @see: L{setForeignKey}
         """
@@ -101,18 +111,18 @@ class AssociationReferenceProxy(dict):
             del self[buffer_key]
 
         # if value is NULL/None or value's primary key is NULL/None
-        # then set associating object's column foreign key value to None
+        # then set referenced object's column foreign key value to None
         fk_value = None
 
         if value is not None:
-            if value.key is None:
-                # associated object's primary key is not defined,
+            if value.__key__ is None:
+                # refernced object's primary key is not defined,
                 # store object in reference buffer
                 dict.__setitem__(self, buffer_key, value)
             else:
-                # set associating object's column foreign key value
-                # to associated object primary key
-                fk_value = value.key
+                # set referenced object's column foreign key value
+                # to referenced object's primary key
+                fk_value = value.__key__
 
         # set foreign key value
         self.setForeignKey(buffer_key, fk_value)
@@ -120,7 +130,7 @@ class AssociationReferenceProxy(dict):
 
     def getForeignKey(self, buffer_key):
         """
-        Abstract method to get foreign key value of associating
+        Abstract method to get foreign key value of referenced
         object's column.
 
         @param buffer_key: Buffer key value.
@@ -130,8 +140,8 @@ class AssociationReferenceProxy(dict):
 
     def setForeignKey(self, buffer_key, value_key):
         """
-        Abstract method to set foreign key value of associating
-        object's column with associated object's primary key.
+        Abstract method to set foreign key value of referenced
+        object's column to referenced object's primary key.
 
         @param buffer_key: Buffer key value.
         @param value_key: Associated object's primary key value.
@@ -140,25 +150,24 @@ class AssociationReferenceProxy(dict):
 
 
 
-class OneToOneAssociation(AssociationReferenceProxy):
+class OneToOne(AssociationReferenceProxy):
     """
-    Base descriptor class for uni and bi-directional one to one association
-    descriptors.
+    Class for uni-directional one-to-one association descriptors.
 
     @see: L{bazaar.assoc.AssociationReferenceProxy}
-    @see: L{bazaar.assoc.UniDirOneToOneAssociation}
-    @see: L{bazaar.assoc.BiDirOneToOneAssociation}
+    @see: L{bazaar.assoc.BiDirOneToOne}
     """
 
     def __get__(self, obj, cls):
         """
-        Descriptor method to get reference of associated object.
+        Descriptor method to retrieve reference of referenced object for
+        application object.
 
-        @param obj: Associating object.
-        @param cls: Associating class.
+        @param obj: Application object.
+        @param cls: Application class.
 
-        @return: Returns associated object when C{obj} is not null,
-            else descriptor object is returned.
+        @return: Returns referenced object when C{obj} is not null,
+            otherwise descriptor object is returned.
         """
         if obj:
             return self[obj]
@@ -168,56 +177,266 @@ class OneToOneAssociation(AssociationReferenceProxy):
 
     def getForeignKey(self, buffer_key):
         """
-        Return foreign key value of associating object's column.
+        Return foreign key value of referenced object's column.
 
-        @param buffer_key: Buffer key value, which is associating object.
+        @param buffer_key: Buffer key value, which is application object.
         """
-        return getattr(buffer_key, self.column.name)
+        return getattr(buffer_key, self.col.col)
 
 
     def setForeignKey(self, buffer_key, value_key):
         """
-        Set foreign key value of associating object's column to primary key
-        of associated object.
+        Set foreign key value of application object's column to
+        referenced object's primary key.
 
-        @param buffer_key: Buffer key value, which is associating object.
-        @param value_key: Associated object's primary key value.
+        @param buffer_key: Buffer key value, which is application object.
+        @param value_key: Referenced object's primary key value.
         """
-        setattr(buffer_key, self.column.name, value_key)
+        setattr(buffer_key, self.col.col, value_key)
 
 
-
-class UniDirOneToOneAssociation(OneToOneAssociation):
-    """
-    Uni-directional one-to-one association descriptor.
-    """
     def __set__(self, obj, value):
         """
-        Descriptor method to set associating object's column value.
+        Descriptor method to set application object's column value.
 
         This method is optimized for uni-directional one-to-one
-        associations.
+        association.
 
-        @param obj: Associating object.
-        @param value: Associated object.
+        @param obj: Application object.
+        @param value: Referenced object.
         """
         self[obj] = value
 
 
 
-class BiDirOneToOneAssociation(OneToOneAssociation):
+class BiDirOneToOne(OneToOne):
     """
     Bi-directional one-to-one association descriptor.
     """
     def __set__(self, obj, value):
         """
-        Descriptor method to set associating object's column value.
+        Descriptor method to set application object's column value.
 
         The method keeps data integrity of bi-directional one-to-one
-        associations.
+        association.
 
-        @param obj: Associating object.
-        @param value: Associated object.
+        @param obj: Application object.
+        @param value: Referenced object.
         """
-        self[obj] = value
-        self.column.association[value] = obj
+        super(BiDirOneToOne, self).__set__(obj, value)
+        self.association.integrate(value, obj)
+
+
+    def integrate(self, obj, value):
+        """
+        Keep bi-directional association data integrity method.
+
+        Method is called by second association object from bi-directional
+        relationship.
+        
+        Application object is referenced object in second association
+        object.
+
+        @param obj: Application object.
+        @param value: Referenced object.
+        """
+        super(BiDirOneToOne, self).__set__(obj, value)
+
+
+
+class ListAssociation(AssociationReferenceProxy):
+    """
+    Basic descriptor for one-to-many and many-to-many associations.
+
+    @ivar obj_lists: Lists of referenced objects per application object.
+    @ivar reload: If true, then association data will be loaded from
+        database.
+    """
+    def __init__(self, col):
+        """
+        Create descriptor for one-to-many and one-to-one associations.
+
+        @param col: Referenced application object's class column.
+
+        @see: L{bazaar.assoc.AssociationReferenceProxy.__init__}
+            L{bazaar.core.Bazaar.__init__} L{bazaar.conf.Persistence}
+            L{bazaar.conf.Column}
+        """
+        super(ListAssociation, self).__init__(col)
+        self.obj_lists = {}
+        self.reload = True
+
+
+
+    def getForeignKey(self, buffer_key):
+        """
+        Return foreign key value of referenced object's column.
+
+        @param buffer_key: Buffer key value, which is pair of application
+            object and index of referenced object in referenced object list.
+        @return: Referenced object's primary key value.
+        """
+        assert len(buffer_key) == 2 and type(buffer_key[0]) == self.broker.cls
+        # obj = buffer_key[0]
+        # index = buffer_key[1]
+        return list.__getitem__(self.obj_lists[buffer_key[0]], buffer_key[1])
+
+
+    def setForeignKey(self, buffer_key, value_key):
+        """
+        Set foreign key value of application object's column to primary key
+        of referenced object.
+
+        @param buffer_key: Buffer key value, which is pair of application
+            object and index of referenced object in object list.
+        @param value_key: Referenced object's primary key value.
+        """
+        assert len(buffer_key) == 2 and type(buffer_key[0]) == self.broker.cls
+        # obj = buffer_key[0]
+        # index = buffer_key[1]
+        list.__setitem__(self.obj_lists[buffer_key[0]], buffer_key[1], value_key)
+
+
+    def __get__(self, obj, cls):
+        """
+        Descriptor method to get list of referenced objects.
+
+        For example, (items is the descriptor)::
+
+            order_item_list = order.items
+
+        @param obj: Application object.
+        @param cls: Application class.
+
+        @return: Returns list of referenced objects, when C{obj} is not null,
+            otherwise descriptor object is returned.
+        """
+        if obj:
+
+            if self.reload:
+                self.loadAssociations()
+
+            if obj not in self.obj_lists:
+                self.obj_lists[obj] = ObjectList(obj, self)
+
+            return self.obj_lists[obj]
+        else:
+            return self
+
+
+    def __set__(self, obj, value):
+        """
+        Assigning list of referenced objects is not implemented yet.
+        """
+        raise NotImplementedError
+
+
+    def add(self, obj, index, value):
+        """
+        Add referenced object to application object's list of referenced
+        objects.
+
+        @param obj: Application object.
+        @param index: Index of the referenced object in the list of
+            referenced objects.
+        @param value: Referenced object.
+        """
+        self[(obj, index)] = value
+
+
+    def append(self, obj, value):
+        """
+        Append referenced object to application object's list of referenced
+        objects.
+
+        @param obj: Application object.
+        @param value: Referenced object.
+        """
+        if obj is not None:                  # None cannot have list of referenced objects
+            if obj not in self.obj_lists:    # if list of referenced objects does not exist, then create it
+                self.obj_lists[obj] = ObjectList(obj, self)
+
+            self.obj_lists[obj].append(value)
+
+
+    def reloadAssociations(self):
+        """
+        Reload association.
+        """
+        self.obj_lists.clear()
+        self.reload = True
+
+
+    def loadAssociations(self):
+        """
+        Load association data from database.
+        """
+        log.info('load association %s.%s' % (self.broker.cls, self.col.attr))
+        for okey, vkey in self.broker.convertor.getAssociationPair(self.col):
+            obj = self.broker.get(okey)
+            value = self.vbroker.get(vkey)
+            self.append(obj, value)
+        log.info('len(%s.%s) = %d' % (self.broker.cls, self.col.attr, len(self.obj_lists)))
+
+        self.reload = False
+
+
+    def update(self, obj_list):
+        """
+        Update association data of given list of referenced objects in
+        database.
+
+        fixme: nfy
+        """
+        afkey = obj_list.obj.__key__
+        for i in range(len(obj_list)):
+            obj = obj_list[i]
+            self.broker.convertor.addAssociationPair(self.col, afkey, obj.__key__)
+            # get from appended list
+
+
+
+
+
+class UniDirManyToMany(ListAssociation):
+    """
+    Uni-directional many-to-many association.
+    """
+    pass
+
+
+class OneToMany(ListAssociation):
+    """
+    Bi-directional one-to-many association descriptor.
+
+    One-to-many association is always bi-directional relationship.
+    """
+    def add(self, obj, index, value):
+        super(OneToMany, self).add(obj, index, value)
+        self.association.integrate(value, obj)
+
+
+    def integrate(self, obj, value):
+        self.append(obj, value)
+
+
+class ObjectList(list):
+    def __init__(self, obj, association):
+        self.obj = obj
+        self.association = association
+
+    def __getitem__(self, item):
+        return self.association[(self.obj, item)]
+
+    def __setitem__(self, index, value):
+        self.association.add(self.obj, index, value)
+
+
+    def append(self, value):
+        index = len(self)
+        list.append(self, None)
+        self.association.add(self.obj, index, value)
+
+
+    def update(self):
+        self.association.update(self)
