f971b49f20fa1dbcbd763f56b880516684a10e03
wrobell <wrobell@pld-linux.org>
2003-09-22 00:30:12 +0000

    - support for new scheme of application class configuration

--- test/app.py
+++ test/app.py
@@ -1,4 +1,4 @@
-# $Id: app.py,v 1.6 2003/09/03 22:30:38 wrobell Exp $
+# $Id: app.py,v 1.7 2003/09/22 00:30:12 wrobell Exp $
 
 """
 Sample test application for Bazaar layer testing purposes.
@@ -11,29 +11,53 @@ import bazaar.conf
 db_module = None
 dsn = ''
 
+####
+#   # simple
+#   .addColumn('price')
+#   .addColumn('price', 'price_col')
+#
+#   # 1-1 uni-dir
+#   .addColumn('boss', 'boss_key', Boss)
+#
+#   # 1-1 bi-dir
+#   .addColumn('boss', 'boss_key', Boss, vattr = 'department')
+#   .addColumn('department', 'dep_key', Department, vattr = 'boss')
+#
+#   # 1-n, bi-dir
+#   addColumn('items', vcls = OrderItem, vcol = 'order_fkey', vattr = 'order')
+#   addColumn('order', 'order_fkey', Order, vattr = 'items')
+#
+#   # 1-n, many side, uni-dir <- it is like 1-1 uni-dir
+#   addColumn('article', article_fkey, Article)
+#
+#   # 1-n, one side, uni-dir <- does not exist
+#
+#   # m-n uni-dir
+#   addColumn('orders', 'employee', Order, 'employee_orders', 'order')
+#
+#   # m-n bi-dir; de facto vattr = 'emploeyees'
+#   addColumn('orders', 'employee' Order, 'employee_orders', 'order',, 'emploeyees')
+#   addColumn('emploeyees', 'order', Employee, 'employee_orders', 'employee', 'orders')
+#
 # define test application classes
+Article = bazaar.conf.Persistence('Article', relation = 'article')
+Article.addColumn('name')
+Article.addColumn('price')
+
 Order = bazaar.conf.Persistence('Order', relation = 'order')
+OrderItem = bazaar.conf.Persistence('OrderItem', relation = 'order_item')
+OrderItem.addColumn('pos')
+OrderItem.addColumn('quantity')
+OrderItem.addColumn('article', 'article_fkey', Article)
+OrderItem.addColumn('order', 'order_fkey', Order, vattr = 'items')
+
 Order.addColumn('no')
 Order.addColumn('finished')
-#Order.addColumn('items')
+Order.addColumn('items', vcls = OrderItem, vcol = 'order_fkey', vattr = 'order')
 #Order.addColumn('employees')
-Order.setKey(('no', ))
 
 Employee = bazaar.conf.Persistence('Employee', relation = 'employee')
 Employee.addColumn('name')
 Employee.addColumn('surname')
 Employee.addColumn('phone')
-#Employee.addColumn('orders')
-Employee.setKey(('name', 'surname'))
-
-Article = bazaar.conf.Persistence('Article', relation = 'article')
-Article.addColumn('name')
-Article.addColumn('price')
-Article.setKey(('name',))
-
-OrderItem = bazaar.conf.Persistence('OrderItem', relation = 'order_item')
-OrderItem.addColumn('order')
-OrderItem.addColumn('pos')
-OrderItem.addColumn('quantity')
-OrderItem.addColumn('article_fkey', 'article', Article, ('article_fkey',))
-OrderItem.setKey(('order', 'pos'))
+Employee.addColumn('orders', 'employee', Order, 'employee_orders', 'order')
