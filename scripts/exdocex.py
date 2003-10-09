fa84614f5404f10d87a8cc39c0daa6a5c7e7bd5d
wrobell <wrobell@pld-linux.org>
2003-10-09 16:41:49 +0000

    - extract documentation examples

---
+++ scripts/exdocex.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+# $Id: exdocex.py,v 1.1 2003/10/09 16:41:49 wrobell Exp $
+
+import sys
+import re
+
+ilen = 4       # amount of indent spaces (yes, no tabs)
+code = False   # processing example code
+indent = None  # indentation of example code 
+
+for l in sys.stdin:
+    if code:
+        if re.match('(^(%s)+[\'\"{}()\[\]#_a-zA-Z])|(^$)' % indent, l) is not None:
+            pl = re.match('(^%s(.*))|^()$' % indent, l).group(2)
+            if pl is None:
+                print
+            else:
+                print pl
+        else: # end of example code
+            code = False
+            indent = None
+
+    # find the begining of example code
+    if l[-3:] == '::\n':    # we should process docstrings, now
+        assert not code and indent is None
+        match = re.match('^ *', l)
+        if match is not None:
+            code = True
+            indent = match.group(0) + ilen * ' '
