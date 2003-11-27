3c4410454b40af6cbdc5f8fcfe5cffeeff1662d8
wrobell <wrobell@pld-linux.org>
2003-11-27 00:38:25 +0000

    - scalability tests

---
+++ test/huge/scale.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import sys
+import time
+import psycopg
+
+import bazaar.core
+
+import app
+
+Order     = app.Order
+OrderItem = app.OrderItem
+Article   = app.Article
+Employee  = app.Employee
+
+mod = __import__(sys.argv[1])
+dsn = sys.argv[2]
+amount = int(sys.argv[3])
+
+bzr = bazaar.core.Bazaar((Order, OrderItem, Article, Employee), \
+    dbmod = mod, dsn = dsn)
+
+art = Article({'name': 'apple', 'price': 2.22})
+bzr.add(art)
+ord = Order({'no': 1, 'finished': False})
+bzr.add(ord)
+
+ts = time.time()
+items = ord.items
+for i in range(amount):
+    oi = OrderItem()
+    oi.article = art
+    oi.quantity = 10
+    oi.pos = i
+    items.append(oi)
+    bzr.add(oi)
+items.update()
+bzr.reloadObjects(OrderItem)
+Order.items.reloadData(True)
+te = time.time()
+print '%5d %0.2f' % (amount, te - ts)
---
+++ test/huge/scale_run.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ $# != 1 ]; then
+    echo usage: $0 "<max>"
+    exit 1
+fi
+
+MAX=$1
+
+for i in $(seq 1000 1000 $MAX); do
+    ./scale.py psycopg 'dbname = ord port = 5433' $i
+done | tee scale.out
+
+gnuplot <<END
+set xlabel 'Ilosc obiektow'
+set ylabel 'Czas[s]'
+set terminal pdf
+set output 'scale.pdf'
+plot "scale.out" with points pointtype 3, 0.005*x+6
+END
