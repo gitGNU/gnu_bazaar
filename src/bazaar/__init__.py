49b87d7a59c7668f0ef30f04edba21a5db1ae47d
wrobell <wrobell@pld-linux.org>
2004-01-22 23:21:40 +0000

    - put license info into source files

--- src/bazaar/__init__.py
+++ src/bazaar/__init__.py
@@ -1,4 +1,25 @@
-# $Id: __init__.py,v 1.9 2004/01/21 23:06:27 wrobell Exp $
+# $Id: __init__.py,v 1.10 2004/01/22 23:21:40 wrobell Exp $
+#
+# Bazaar - an easy to use and powerful abstraction layer between relational
+# database and object oriented application.
+#
+# Copyright (C) 2000-2004 by Artur Wroblewski <wrobell@pld-linux.org>
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
 """
 Bazaar is an easy to use and powerful abstraction layer between
 relational database and object oriented application.
--- test/huge/bzr.py
+++ test/huge/bzr.py
@@ -1,4 +1,25 @@
-# $Id: bzr.py,v 1.2 2003/11/27 15:20:06 wrobell Exp $
+# $Id: bzr.py,v 1.3 2004/01/22 23:21:41 wrobell Exp $
+#
+# Bazaar - an easy to use and powerful abstraction layer between relational
+# database and object oriented application.
+#
+# Copyright (C) 2000-2004 by Artur Wroblewski <wrobell@pld-linux.org>
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
 import optparse
 import sys
 import time
